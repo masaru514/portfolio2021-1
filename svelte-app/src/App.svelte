cfb2fdf37d61791ac5e8e3bd40ae11bd4141f582
masaru514 <415urasam@gmail.com>
2021-01-23 00:59:34 +0900

    init

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Tailwindcss from "./Tailwindcss.svelte";
+
+  export let name;
+</script>
+
+<Tailwindcss />
+<main>
+  <h1 class="mt-60">Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
+
+<style lang="scss">
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    color: #333;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
